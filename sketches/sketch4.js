763099239d839b92024cceeafe86ea1752e32722
Daphni <daphng@uw.edu>
2025-10-27 22:30:00 -0700

    added the flower array for sketch 4

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -1,11 +1,25 @@
 registerSketch('sk4', function (p) {
+  let flowers = [];
+  let lastMinute = -1;
+
   p.setup = function () {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(150, 170, 140); // sage green
+    p.noStroke();
+    p.textAlign(p.LEFT, p.TOP);
+    p.textSize(16);
   };
 
   p.draw = function () {
     p.background(150, 170, 140);
+
+    let h = p.hour() % 12;
+    if (h === 0) h = 12;
+    let m = p.minute();
+
+    if (m !== lastMinute) {
+      lastMinute = m;
+      flowers.push({ x: p.random(p.width), y: p.random(p.height) });
+    }
   };
 
   p.windowResized = function () {
